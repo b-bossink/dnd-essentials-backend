b3460b22b5b978cf23dea3022e43e35f9cd8cc8e
Boris Bossink <b.bossink@student.fontys.nl>
2022-09-27 14:23:14 +0200

    Created a way to get test characters via HTTPS.
    Added a project that holds models for entities and an 'Authentication' project, which will be used for user-related stuff.

---
+++ API/Controllers/CharacterController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CampaignData.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CampaignData.Controllers
+{
+    [Route("api/[controller]")]
+    public class CharacterController : Controller
+    {
+        private static IEnumerable<Character> _all = new Character[]
+        {
+            new Character(1, "My First Character", "Knight", "Monster", 1, 2 ,3 ,4, 5, 6),
+            new Character(2, "A Cool Character", "Alchemist", "Human", 6, 5 ,4 ,3, 2, 1)
+        };
+
+        // GET: api/character
+        [HttpGet]
+        public IEnumerable<Character> Get()
+        {
+            return _all;
+        }
+
+        // GET api/character/{id}
+        [HttpGet("{id}")]
+        public Character Get(int id)
+        {
+            var results = _all.Where(c => c.ID == id).ToList();
+            if (results.Count >= 1) {
+                return results[0];
+            }
+            return null;
+        }
+
+        // POST api/character
+        [HttpPost]
+        public void Post([FromBody] Character value)
+        {
+        }
+
+        // PUT api/character
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Character value)
+        {
+        }
+
+        // DELETE api/character
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
