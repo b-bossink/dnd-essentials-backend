b1b38ce12e28ff607cfea5c920265e4a768edd1b
Boris Bossink <b.bossink@student.fontys.nl>
2022-11-08 08:56:24 +0100

    Added GenerationService class

---
+++ Service/CharacterGeneration/GenerationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Service.CharacterGeneration
+{
+	public class GenerationService
+	{
+		private int RollDice(int eyes)
+		{
+			return new Random().Next(0, eyes);
+		}
+	}
+}
+
